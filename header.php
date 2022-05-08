139bb0351fb56fc482281adbfdfb8fdf1ceddb4e
Anton Maslennikov <anton.maslennikov@gmail.com>
2022-05-08 12:48:20 +0300

    +

--- header.php
+++ header.php
@@ -26,19 +26,19 @@
     }
     ?>
 
-    <link rel="apple-touch-icon" sizes="57x57" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/smaller-blue-flat@3x.png">
     <link rel="manifest" href="/wp-content/themes/<?= get_template() ?>/assets/images/favicons/manifest.json">
 
     <meta name="msapplication-TileColor" content="#8ed0ef">
