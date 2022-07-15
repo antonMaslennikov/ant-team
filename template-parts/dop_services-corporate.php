f3f7309b656b23231e27ca563e2685bc066061c5
Anton Maslennikov <anton.maslennikov@gmail.com>
2022-07-15 16:59:20 +0300

    n

--- template-parts/dop_services-corporate.php
+++ template-parts/dop_services-corporate.php
@@ -12,7 +12,7 @@
                                 <div class="tariff__header">
                                     <h3 class="dop_service__title">Индивидуальные SEO-консультации</h3><span class="dop_service__price">от 5 000 ₽</span>
                                 </div>
-                                <div class="dop_service__content -yml">
+                                <div class="dop_service__content">
                                     <div class="dop_service__desc">Вам не нужно полноценное продвижение, а достаточно качественной консультации? Квалифицированные SEO-специалисты из Ant-Team.ru помогут вам в формате разового онлайн-созвона или предложат поддержку и кураторство в течение длительного времени!</div>
                                     <a class="btn btn-small dop_service__link" data-fancybox data-dop_service="Индивидуальные SEO-консультации" href="#modal-order-form">Заказать</a>
                                 </div>
@@ -24,7 +24,7 @@
                                     <h3 class="dop_service__title">Продвижение сайта</h3><span class="dop_service__price">от 50 000 ₽</span>
                                 </div>
                                 <div class="dop_service__content -yml">
-                                    <div class="dop_service__desc">Возьмём ваш сайт на постоянное SEO-продвижение под Google и Яндекс. Полный спектр работ: от сбора и проработки семантики до аналитики и планирования.<br><br></div>
+                                    <div class="dop_service__desc">Возьмём ваш сайт на постоянное SEO-продвижение под Google и Яндекс. Полный спектр работ: от сбора и проработки семантики до аналитики и планирования.</div>
                                     <a class="btn btn-small dop_service__link" data-fancybox data-dop_service="Продвижение сайта" href="#modal-order-form">Заказать</a>
                                 </div>
                             </div>
@@ -35,7 +35,7 @@
                                     <h3 class="dop_service__title"><a href="/tehnicheski-audit/">Технический аудит</a></h3><span class="dop_service__price">от 30 000 ₽</span>
                                 </div>
                                 <div class="dop_service__content -yml">
-                                    <div class="dop_service__desc">Проверим ваш сайт на наличие технических ошибок и составим подробные ТЗ на исправление. Поможем в реализации при необходимости.<br><br></div>
+                                    <div class="dop_service__desc">Проверим ваш сайт на наличие технических ошибок и составим подробные ТЗ на исправление. Поможем в реализации при необходимости.</div>
                                     <a class="btn btn-small dop_service__link" data-fancybox data-dop_service="Технический аудит" href="#modal-order-form">Заказать</a>
                                 </div>
                             </div>
